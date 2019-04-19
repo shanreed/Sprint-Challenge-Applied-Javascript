5032ba5ca0501ed369df6b55f27a76843cfe8340
shanreed <srtech5set@gmail.com>
2019-04-19 09:34:04 -0700

    created forEach method for the selectTab method

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -15,9 +15,8 @@ class TabLink {
       this.cards = document.querySelectorAll('.cards-container .card');
     } else {
       // else if `all` is false, only select the cards with matching this.tabData values
-     this.cards= document.querySelectorAll(`.card[data-tab = '${this.tabData}']`)
-    
-
+     this.cards= document.querySelectorAll(`.card[data-tab = '${this.tabData}']`);
+    }
      // Map over the newly converted NodeList we just created in our if statement above. 
      //Convert each this.cards element into a new instance of the TabCard class. 
      //Pass in a card object to the TabCard class. 
@@ -25,21 +24,24 @@ class TabLink {
 
     // Add a click event that invokes this.selectTab
      this.tabElement.addEventListener('click', () => {
-       this.selectTab()
-       console.log('working');
+       this.selectTab();
+       //console.log('working');
      });
   }
 
   selectTab(){
 
     // Select all elements with the .tab class on them
-    // const tabs = document.querySelectorAll();
+     const tabs = document.querySelectorAll('.tab');
+     console.log(tabs)
     
     // Iterate through the NodeList removing the .active-tab class from each element
-    // tabs.forEach()
+     tabs.forEach(function(tab){
+       tab.ClassList.remove('.active-tab')
+     })
 
     // Select all of the elements with the .card class on them
-    // const cards = ;
+     const cards = document.querySelectorAll('.cards-container .card') ;
 
     // Iterate through the NodeList setting the display style each one to 'none'
     // cards.forEach()
