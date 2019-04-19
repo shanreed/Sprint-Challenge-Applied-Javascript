b821fcee58f52d6fe03c0fc51f8265f9871d8e1f
shanreed <srtech5set@gmail.com>
2019-04-19 09:12:52 -0700

    fixed instruction for the if statement in Tabs.js file

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -12,10 +12,10 @@ class TabLink {
     // Check to see if this.tabData is equal to 'all'
     if(this.tabData === 'all'){
       // If `all` is true, select all cards regardless of their data attribute values
-      this.cards = document.querySelectorAll('.card');
+      this.cards = document.querySelectorAll('.cards-container .card');
     } else {
       // else if `all` is false, only select the cards with matching this.tabData values
-     this.cards= document.querySelectorAll(`.card[data-tab '${this.tabData}']`)
+     this.cards= document.querySelectorAll(`.card[data-tab = '${this.tabData}']`)
     
 
      // Map over the newly converted NodeList we just created in our if statement above. Convert each this.cards element into a new instance of the TabCard class. Pass in a card object to the TabCard class. 
